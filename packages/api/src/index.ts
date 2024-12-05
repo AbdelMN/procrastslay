9b951811862bf2cfae9e6e2ae0789fb9730f7288
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2024-12-05 23:02:28 +0100

    feat: start notion task list import

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,11 +3,12 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import authRoute from './auth/auth';
 import callbackRoute from './auth/notionCallback';
+import notionImportRoute from './notion/importTaskList';
 const app = new Hono({ strict: false }).basePath('/');
 
 app.route('/auth/', authRoute);
 app.route('/auth/callback', callbackRoute);
-
+app.route('/notion/import', notionImportRoute);
 const port = 3000;
 
 serve({
---
+++ packages/api/src/notion/importTaskList.ts
@@ -0,0 +1,33 @@
+import { Hono } from 'hono';
+import { getCookie } from 'hono/cookie';
+const app = new Hono();
+import { validateSessionToken } from '../auth/sessions';
+app.get('/', async (c) => {
+  const session = getCookie(c, 'session');
+  if (session) {
+    const user = await validateSessionToken(session);
+    if (user.user) {
+      const accessToken = user.user.notionAccessToken;
+      const dbResponse = await fetch('https://api.notion.com/v1/search', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+          'Notion-Version': '2022-06-28',
+        },
+        body: JSON.stringify({
+          filter: {
+            property: 'object',
+            value: 'database',
+          },
+        }),
+      });
+
+      const dbData = await dbResponse.json();
+
+      console.log(dbData.results);
+    }
+  }
+});
+
+export default app;
