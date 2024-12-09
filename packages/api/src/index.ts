7f8be47c95786a66a791d243b208721e2e81bc26
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2024-12-09 23:29:26 +0100

    feat(api): add route to get all tasklist of the user

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,11 +4,13 @@ import { Hono } from 'hono';
 import authRoute from './auth/auth';
 import callbackRoute from './auth/notionCallback';
 import notionImportRoute from './notion/importTaskList';
+import notionTaskListRoute from './taskList';
 const app = new Hono({ strict: false }).basePath('/');
 
 app.route('/auth/', authRoute);
 app.route('/auth/callback', callbackRoute);
 app.route('/notion/import', notionImportRoute);
+app.route('notion/tasklist', notionTaskListRoute);
 const port = 3000;
 
 serve({
---
+++ packages/api/src/taskList.ts
@@ -0,0 +1,22 @@
+import { Hono } from 'hono';
+import { getCookie } from 'hono/cookie';
+const app = new Hono();
+import { validateSessionToken } from './auth/sessions';
+import { prisma } from './prisma';
+
+app.get('/', async (c) => {
+  const session = getCookie(c, 'session');
+  if (session) {
+    const user = await validateSessionToken(session);
+    if (user.user) {
+      const result = await prisma.databases.findMany({
+        where: {
+          userId: user.user.id,
+        },
+      });
+      console.log(result);
+    }
+  }
+});
+
+export default app;
