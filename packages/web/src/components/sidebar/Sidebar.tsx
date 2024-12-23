0b9388eea63d60627d4f02d53e326fdb6b8c71b1
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2024-12-23 17:17:51 +0100

    feat(web): add sidebar nav component

---
+++ packages/web/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,53 @@
+import { Box, Button, VStack } from '@chakra-ui/react';
+import { FaHouse, FaSquarePollVertical, FaClock } from 'react-icons/fa6';
+import { Separator } from '@chakra-ui/react';
+const SidebarMenu = () => (
+  <VStack>
+    <Button
+      w="100%"
+      bg={'transparent'}
+      _hover={{ bg: 'gray.700' }}
+      justifyContent={'left'}
+    >
+      <FaHouse /> Dashboard
+    </Button>
+    <Button
+      w="100%"
+      bg={'transparent'}
+      _hover={{ bg: 'gray.700' }}
+      justifyContent={'left'}
+    >
+      <FaSquarePollVertical />
+      Habits
+    </Button>
+    <Button
+      w="100%"
+      bg={'transparent'}
+      _hover={{ bg: 'gray.700' }}
+      justifyContent={'left'}
+    >
+      <FaClock />
+      Pomodoro
+    </Button>
+  </VStack>
+);
+
+const Sidebar = () => {
+  return (
+    <Box
+      as="nav"
+      position="sticky"
+      left={0}
+      p={5}
+      w="200px"
+      top={0}
+      h="100vh"
+      bg="#dfdfdf"
+    >
+      <SidebarMenu />
+      <Separator />
+    </Box>
+  );
+};
+
+export default Sidebar;
--- packages/web/src/routes/_auth.dashboard.tsx
+++ packages/web/src/routes/_auth.dashboard.tsx
@@ -2,37 +2,24 @@ import { createFileRoute } from '@tanstack/react-router';
 import TaskList from '../components/tasks/tasklist';
 import { useAuth } from '../auth';
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+
+import { Box, Flex } from '@chakra-ui/react';
 export const Route = createFileRoute('/_auth/dashboard')({
   component: DashboardPage,
 });
 import ky from 'ky';
-
-const fetchSession = async () => {
-  const response = await ky('http://localhost:3000/notion/tasklist', {
-    credentials: 'include',
-  });
-
-  return response.json();
-};
 
 function DashboardPage() {
   const auth = useAuth();
   if (!auth.user) {
     return <p> Vous n'êtes pas connecté </p>;
   }
-  const { data, isError, isLoading, error } = useQuery({
-    queryKey: ['ssa'],
-    queryFn: fetchSession,
-  });
-  if (isLoading) {
-    console.log('Chargement en cours...');
-    return <div>Chargement...</div>;
-  }
 
-  if (isError) {
-    console.error('Erreur de chargement des données :', error);
-    return <div>Erreur : Impossible de récupérer les données</div>;
-  }
-  console.log(data);
-  return <TaskList tasklist={data} />;
+  return (
+    <Box p="4">
+      <h1>Authenticated route</h1>
+      <p>Welcome to the dashboard</p>
+    </Box>
+  );
 }
