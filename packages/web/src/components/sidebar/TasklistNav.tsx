77b7a211eb8364a040a124e22f601b525e1de8d8
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2024-12-25 13:08:38 +0100

    feat(web): show takslist from the db and add tasklist to the db with a form in a dialog

--- packages/web/src/components/sidebar/TasklistNav.tsx
+++ packages/web/src/components/sidebar/TasklistNav.tsx
@@ -1,15 +1,58 @@
-import { Flex, Box, Button, Collapsible } from '@chakra-ui/react';
-import { FaChevronRight, FaChevronDown } from 'react-icons/fa6';
+import { Flex, Box, Button, Collapsible, Input, Stack } from '@chakra-ui/react';
+import { FaChevronRight, FaChevronDown, FaCirclePlus } from 'react-icons/fa6';
 import { useState } from 'react';
+import ky from 'ky';
+import { useQuery } from '@tanstack/react-query';
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Field } from '@/components/ui/field';
+import { useForm } from 'react-hook-form';
+const fetchTasklist = async () => {
+  const response = await ky('http://localhost:3000/tasklist', {
+    credentials: 'include',
+  });
+  return response.json();
+};
 
-const tasklist = [
-  { id: 1, title: 'Work', userId: 1 },
-  { id: 2, title: 'Personal', userId: 1 },
-  { id: 3, title: 'Study', userId: 1 },
-];
+const postTaskList = async (title: string) => {
+  const response = await ky
+    .post('http://localhost:3000/tasklist', {
+      credentials: 'include',
+      json: { title: title },
+    })
+    .json();
+  return response;
+};
+
 const TaskListNav = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const { data, refetch } = useQuery({
+    queryKey: ['tasklist'],
+    queryFn: fetchTasklist,
+    enabled: true,
+    retry: false,
+  });
 
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      tasklistName: '',
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    postTaskList(data.tasklistName);
+
+    setIsDialogOpen(false);
+    refetch();
+  });
   return (
     <Collapsible.Root
       onOpenChange={(open) => {
@@ -21,13 +64,46 @@ const TaskListNav = () => {
         <Flex align="center">
           {isOpen ? <FaChevronDown /> : <FaChevronRight />}
           <Box ml="2">Task List</Box>
+          <DialogRoot
+            open={isDialogOpen}
+            onOpenChange={(isOpen) => setIsDialogOpen(isOpen.open)}
+            placement="center"
+          >
+            <DialogTrigger asChild>
+              <FaCirclePlus
+                onClick={(event) => {
+                  event.stopPropagation();
+                }}
+              />
+            </DialogTrigger>
+            <DialogContent
+              onClick={(event) => {
+                event.stopPropagation();
+              }}
+            >
+              <DialogHeader>
+                <DialogTitle>Add a tasklist</DialogTitle>
+              </DialogHeader>
+              <DialogBody pb="8">
+                <form onSubmit={onSubmit}>
+                  <Stack gap="4" align="flex-start" maxW="sm">
+                    <Field label="Tasklist Name">
+                      <Input {...register('tasklistName')} />
+                    </Field>
+
+                    <Button type="submit">Confirm </Button>
+                  </Stack>
+                </form>
+              </DialogBody>
+              <DialogCloseTrigger />
+            </DialogContent>
+          </DialogRoot>
         </Flex>
       </Collapsible.Trigger>
       <Collapsible.Content>
         <Flex direction={'column'}>
-          {tasklist.map((list) => (
-            <Button>{list.title}</Button>
-          ))}
+          {data && // Vérifie que data existe avant de mapper
+            data.map((list) => <Button key={list.id}>{list.title}</Button>)}
         </Flex>
       </Collapsible.Content>
     </Collapsible.Root>
