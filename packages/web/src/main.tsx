d4636af6e81cd385e35ab40d484632b4d855c3d1
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-01-07 15:52:29 +0100

    fix(web): remove unused import

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { Provider } from '@/components/ui/provider';
-import {
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 import { CookiesProvider } from 'react-cookie';
 import { routeTree } from './routeTree.gen';
