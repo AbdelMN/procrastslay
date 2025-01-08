4a9a051e3631de657b5a674fa1f86394c08177df
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-01-08 18:47:56 +0100

    chore(web): put tasklist page logic in tasklist route file

--- packages/web/src/routes/_auth/tasklist/$tasklistId.tsx
+++ packages/web/src/routes/_auth/tasklist/$tasklistId.tsx
@@ -1,15 +1,37 @@
-import TaskList from '@/components/tasks/TaskList';
-import { createFileRoute } from '@tanstack/react-router';
-import { Center } from '@chakra-ui/react';
+import {
+  createFileRoute,
+  Link,
+  Outlet,
+  useLocation,
+} from '@tanstack/react-router';
+import { Box, Center, Spinner } from '@chakra-ui/react';
+import { getTasksQuery } from '@/queries/task';
+import { useQuery } from '@tanstack/react-query';
+import Task from '@/components/tasks/Task';
 export const Route = createFileRoute('/_auth/tasklist/$tasklistId')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
   const { tasklistId } = Route.useParams();
+  const { data, isPending, isError } = useQuery(getTasksQuery(tasklistId));
+  const location = useLocation();
+  if (isPending || isError) return <Spinner />;
   return (
     <Center>
-      <TaskList id={tasklistId} />
+      <Box>
+        <Box width={'500px'}>
+          {data.map((task) => (
+            <Task task={task} key={task.id} />
+          ))}
+        </Box>
+        {!(location.pathname === `/tasklist/${tasklistId}/task/new`) ? (
+          <Link to={`/tasklist/${tasklistId}/task/new`}>+ Add task</Link>
+        ) : (
+          ''
+        )}
+        <Outlet />
+      </Box>
     </Center>
   );
 }
