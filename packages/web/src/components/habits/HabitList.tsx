a6ec10ca982b998177c583bc9d704259e7b02fc6
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-01-14 22:14:44 +0100

    feat(web): show habits from mocked data

--- packages/web/src/components/habits/HabitList.tsx
+++ packages/web/src/components/habits/HabitList.tsx
@@ -1,6 +1,68 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Card, Heading, ProgressCircleRoot } from '@chakra-ui/react';
+import { Checkbox } from '@/components/ui/checkbox';
+import { ProgressCircleRing } from '../ui/progress-circle';
+const mockData = [
+  {
+    id: 'habit-1',
+    userId: '1',
+    name: 'Drink Water',
+    frequencyType: 'daily',
+    frequencyValue: null,
+    days: '["Monday", "Wednesday", "Friday"]',
+    completionMode: 'oneTime',
+    goalValue: null,
+    unit: 'cups',
+    createdAt: '2025-01-01T08:00:00Z',
+  },
+  {
+    id: 'habit-2',
+    userId: '1',
+    name: 'Workout',
+    frequencyType: 'weekly',
+    frequencyValue: 3,
+    days: null,
+    completionMode: 'cumulative',
+    goalValue: 3,
+    unit: 'seances',
+    createdAt: '2025-01-02T08:00:00Z',
+  },
+  {
+    id: 'habit-3',
+    userId: '1',
+    name: 'Read a Book',
+    frequencyType: 'interval',
+    frequencyValue: 3,
+    days: null,
+    completionMode: 'cumulative',
+    goalValue: 20,
+    unit: 'pages',
+    createdAt: '2025-01-03T08:00:00Z',
+  },
+];
 
 const HabitList = () => {
-  return <Box> Habit </Box>;
+  return (
+    <>
+      {mockData.map((habit) => {
+        const isCumulative = habit.completionMode == 'cumulative';
+        return (
+          <Card.Root size="sm">
+            <Card.Header>
+              <Heading size="md"> {habit.name} </Heading>
+            </Card.Header>
+            <Card.Body color="fg.muted">
+              {isCumulative ? (
+                <ProgressCircleRoot size={'xs'} value={100}>
+                  <ProgressCircleRing />
+                </ProgressCircleRoot>
+              ) : (
+                <Checkbox />
+              )}
+            </Card.Body>
+          </Card.Root>
+        );
+      })}
+    </>
+  );
 };
 export default HabitList;
--- packages/web/src/routes/_auth/habits/index.tsx
+++ packages/web/src/routes/_auth/habits/index.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router';
 import HabitList from '@/components/habits/HabitList';
+import { Box, Center } from '@chakra-ui/react';
 export const Route = createFileRoute('/_auth/habits/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <HabitList />;
+  return (
+    <Center>
+      <Box width={'500px'}>
+        <HabitList />
+      </Box>
+    </Center>
+  );
 }
