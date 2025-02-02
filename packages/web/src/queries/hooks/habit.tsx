c521a79541a53c824b298541fe4102acd954ab6f
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-02-02 12:41:44 +0100

    feat(web): add patch and delete queries for tansktack

--- packages/web/src/queries/hooks/habit.tsx
+++ packages/web/src/queries/hooks/habit.tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import ky from 'ky';
+import { HabitType } from '../habit';
 
 const completeHabit = async ({
   habitId,
@@ -9,9 +10,8 @@ const completeHabit = async ({
   habitId: string;
   count: number;
   date: Date;
-}) => {
-  console.log(habitId);
-  const reponse = await ky
+}) =>
+  await ky
     .post('http://localhost:3000/habit/complete', {
       credentials: 'include',
       json: {
@@ -21,9 +21,27 @@ const completeHabit = async ({
       },
     })
     .json();
-  return reponse;
-};
 
+const patchHabit = async (habit: HabitType & { id: string }) =>
+  await ky
+    .patch(`http://localhost:3000/habit/${habit.id}`, {
+      credentials: 'include',
+      json: {
+        ...habit,
+      },
+    })
+    .json();
+
+const postDeleteHabit = async (habitId: string) => {
+  const response = await ky
+    .post('http://localhost:3000/habit/delete', {
+      credentials: 'include',
+      json: { id: habitId },
+    })
+    .json();
+
+  return response;
+};
 export const useCompleteHabit = (day?: Date) => {
   const queryClient = useQueryClient();
   console.log(day);
@@ -32,6 +50,31 @@ export const useCompleteHabit = (day?: Date) => {
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: day ? ['habits', day] : ['habits'],
+      });
+    },
+  });
+};
+
+export const useEditHabit = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: patchHabit,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['habits'],
+      });
+    },
+  });
+};
+
+export const useDeleteHabit = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: postDeleteHabit,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['habits'],
       });
     },
   });
