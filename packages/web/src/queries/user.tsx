bcb074b377c9d3b9c68689fa2ec018baa01dc210
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-02-19 20:33:59 +0100

    fix(web): fix types

--- packages/web/src/queries/user.tsx
+++ packages/web/src/queries/user.tsx
@@ -1,8 +1,15 @@
 import { queryOptions } from '@tanstack/react-query';
 import ky from 'ky';
 
+type UserInfosType = {
+  fuel: number;
+  streak: number;
+};
+
 const fetchUserInfos = async () =>
-  ky('http://localhost:3000/user/infos', { credentials: 'include' }).json();
+  ky('http://localhost:3000/user/infos', {
+    credentials: 'include',
+  }).json() as unknown as UserInfosType;
 
 export const userInfosQuery = queryOptions({
   queryKey: ['userInfos'],
