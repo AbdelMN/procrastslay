2c35b7392d5a9b507499d6cffa4f4f86a6bba360
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-02-19 20:33:08 +0100

    feat(api): add a fn to add or remove user fuel if he complete or uncomplete an habit or a task

--- packages/api/src/services/userService.ts
+++ packages/api/src/services/userService.ts
@@ -10,3 +10,35 @@ export const addUserFuel = async (userId: string, amount: number) => {
     },
   });
 };
+
+const removeUserFuel = async (
+  userId: string,
+  amount: number,
+  completedAt: Date,
+) => {
+  const isCompletionToday =
+    new Date(completedAt).toISOString().split('T')[0] ===
+    new Date().toISOString().split('T')[0];
+
+  if (isCompletionToday) {
+    return await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        fuel: { decrement: amount },
+      },
+    });
+  }
+};
+
+export const addOrRemoveUserFuel = async (
+  completion: Boolean,
+  userId: string,
+  amount: number,
+  completedAt?: Date,
+) => {
+  if (!completion && completedAt)
+    return removeUserFuel(userId, amount, completedAt);
+  return await addUserFuel(userId, amount);
+};
