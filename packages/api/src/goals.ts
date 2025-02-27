bbff6d4ce8a457a27c9aa76105a92c9200a684b0
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-02-27 21:56:47 +0100

    feat(api): post goal route to create  a goal

---
+++ packages/api/src/goals.ts
@@ -0,0 +1,45 @@
+import { zValidator } from '@hono/zod-validator';
+import { Hono } from 'hono';
+import { z } from 'zod';
+import sessionMiddleware from './auth/sessionMiddleware';
+import { prisma } from './prisma';
+
+const app = new Hono();
+
+const GoalSchema = z.object({
+  pomodoro: z
+    .array(z.object({ duration: z.string(), goal: z.number() }))
+    .optional(),
+  habit: z.number(),
+  task: z
+    .array(z.object({ difficulty: z.string(), goal: z.number() }))
+    .optional(),
+  date: z.string(),
+});
+
+app.post('/', zValidator('json', GoalSchema), sessionMiddleware, async (c) => {
+  const receivedGoal = c.req.valid('json');
+
+  const user = c.get('user');
+
+  if (user) {
+    const userId = user.id;
+    const { pomodoro, habit, task, date } = receivedGoal;
+
+    const goal = await prisma.goals.create({
+      data: {
+        userId,
+        pomodoro: pomodoro
+          ? pomodoro.map((pomodoro) => JSON.stringify(pomodoro))
+          : [],
+        habit,
+        task: task ? task.map((task) => JSON.stringify(task)) : [],
+        date: new Date(date),
+      },
+    });
+
+    return c.json(goal);
+  }
+});
+
+export default app;
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -7,6 +7,7 @@ import taskRoute from './task';
 import taskListRoute from './taskList';
 import habitRoute from './habits';
 import userRoute from './user';
+import goalsRoute from './goals';
 
 const app = new Hono({ strict: false }).basePath('/');
 
@@ -16,6 +17,7 @@ app.route('/task', taskRoute);
 app.route('/tasklist', taskListRoute);
 app.route('/habit', habitRoute);
 app.route('/user', userRoute);
+app.route('/goals', goalsRoute);
 
 const port = 3000;
 
