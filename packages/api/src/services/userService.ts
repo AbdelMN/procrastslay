c58f20ada6b9819eb19c0d10e0581bad5bcaca50
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-03-13 16:04:25 +0100

    feat(api): add fn to get user fuel and update existing logic in userService

--- packages/api/src/services/userService.ts
+++ packages/api/src/services/userService.ts
@@ -26,25 +26,15 @@ export const addUserFuel = async (
   });
 };
 
-const removeUserFuel = async (
-  userId: string,
-  amount: number,
-  completedAt: Date,
-) => {
-  const isCompletionToday =
-    new Date(completedAt).toISOString().split('T')[0] ===
-    new Date().toISOString().split('T')[0];
-
-  if (isCompletionToday) {
-    return await prisma.user.update({
-      where: {
-        id: userId,
-      },
-      data: {
-        fuel: { decrement: amount },
-      },
-    });
-  }
+const removeUserFuel = async (userId: string, amount: number) => {
+  return await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      fuel: { decrement: amount },
+    },
+  });
 };
 
 export const addOrRemoveUserFuel = async (
@@ -54,9 +44,30 @@ export const addOrRemoveUserFuel = async (
   completedAt?: Date,
   type?: string,
 ) => {
-  if (!completion && completedAt)
-    return removeUserFuel(userId, amount, completedAt);
-  return await addUserFuel(userId, amount, completedAt, type);
+  if (!completion) {
+    if (completedAt) {
+      const isCompletionToday =
+        new Date(completedAt).toISOString().split('T')[0] ===
+        new Date().toISOString().split('T')[0];
+
+      if (isCompletionToday) return removeUserFuel(userId, amount);
+    }
+    return await addUserFuel(userId, amount, completedAt, type);
+  }
+  if (!completion) {
+    return removeUserFuel(userId, amount);
+  }
+};
+
+export const getUserFuel = async (userId: string) => {
+  const result = await prisma.user.findFirst({
+    where: { id: userId },
+    select: { fuel: true },
+  });
+  if (result) {
+    return result.fuel;
+  }
+  return 0;
 };
 
 export const addStreak = async (userId: string) => {
