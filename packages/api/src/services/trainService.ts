1dadbef09f111b16143d3c58b8d9c494733b5eb0
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-03-13 15:29:03 +0100

    fix(api): fix forgotten "}"

--- packages/api/src/services/trainService.ts
+++ packages/api/src/services/trainService.ts
@@ -1,5 +1,8 @@
 import { prisma } from '../prisma';
 
-export const addTrainFuel = async (userId: string, amount : number ) => {
-    return await prisma.train.update({where: {userId}, data: { fuel: {increment : amount}})
-}
+export const addTrainFuel = async (userId: string, amount: number) => {
+  return await prisma.train.update({
+    where: { userId },
+    data: { fuel: { increment: amount } },
+  });
+};
