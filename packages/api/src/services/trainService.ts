fb9e2e5642ad95fe1f89f3bcd35a2a2eb3ac2972
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-03-13 15:16:19 +0100

    feat(api): add train method to add fuel

---
+++ packages/api/src/services/trainService.ts
@@ -0,0 +1,5 @@
+import { prisma } from '../prisma';
+
+export const addTrainFuel = async (userId: string, amount : number ) => {
+    return await prisma.train.update({where: {userId}, data: { fuel: {increment : amount}})
+}
