aaa7c5c9e24e476d2354c5989886ab5ea8e4c951
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-03-19 20:07:26 +0100

    feat(web): manage empty active goal state

--- packages/web/src/components/goals/Goal.tsx
+++ packages/web/src/components/goals/Goal.tsx
@@ -1,31 +1,43 @@
 import { getCurrentGoal } from '@/queries/goal';
-import { Button, Card, HStack, Progress, Spinner } from '@chakra-ui/react';
+import {
+  Button,
+  Card,
+  EmptyState,
+  HStack,
+  Progress,
+  Spinner,
+  VStack,
+} from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
-
+import { HiColorSwatch } from 'react-icons/hi';
+import GoalForm from './GoalForm';
 const Goal = () => {
   const { data, isPending, isError } = useQuery(getCurrentGoal());
 
   if (isPending || isError) return <Spinner />;
 
-  const progressData = [
-    ...data.task.map((task) => ({
-      label: `${task.difficulty} task`,
-      completed: task.completed,
-      goal: task.goal,
-    })),
-    ...data.pomodoro.map((pomo) => ({
-      label: `${pomo.duration} min pomodoro`,
-      completed: pomo.completed,
-      goal: pomo.goal,
-    })),
-    ...data.habit.map((habit) => ({
-      label: 'Habit',
-      completed: habit.completed,
-      goal: habit.goal,
-    })),
-  ];
+  const progressData =
+    Object.keys(data).length > 0
+      ? [
+          ...data.task.map((task) => ({
+            label: `${task.difficulty} task`,
+            completed: task.completed,
+            goal: task.goal,
+          })),
+          ...data.pomodoro.map((pomo) => ({
+            label: `${pomo.duration} min pomodoro`,
+            completed: pomo.completed,
+            goal: pomo.goal,
+          })),
+          ...data.habit.map((habit) => ({
+            label: 'Habit',
+            completed: habit.completed,
+            goal: habit.goal,
+          })),
+        ]
+      : undefined;
 
-  return (
+  return progressData ? (
     <Card.Root width="320px">
       <Card.Body gap="2">
         <Card.Title mt="2">03/03/2025 - Goal</Card.Title>
@@ -51,6 +63,24 @@ const Goal = () => {
         <Button variant="outline">Cancel goal</Button>
         <Button>Complete day</Button>
       </Card.Footer>
+    </Card.Root>
+  ) : (
+    <Card.Root width="320px">
+      <EmptyState.Root>
+        <EmptyState.Content>
+          <EmptyState.Indicator>
+            <HiColorSwatch />
+          </EmptyState.Indicator>
+          <VStack textAlign="center">
+            <EmptyState.Title>No goals found</EmptyState.Title>
+            <EmptyState.Description>
+              Add a new goal to get started
+            </EmptyState.Description>
+          </VStack>
+
+          <GoalForm />
+        </EmptyState.Content>
+      </EmptyState.Root>
     </Card.Root>
   );
 };
