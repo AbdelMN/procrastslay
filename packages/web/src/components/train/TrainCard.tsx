83a2f08c44eac2b7263ae5c524c53c2854ec6bcb
AbdelMN <55888251+AbdelMN@users.noreply.github.com>
2025-03-22 20:14:56 +0100

    feat(web): use data from api for train's fuel & distance infos

--- packages/web/src/components/train/TrainCard.tsx
+++ packages/web/src/components/train/TrainCard.tsx
@@ -1,9 +1,15 @@
-import { Card, HStack, VStack } from '@chakra-ui/react';
+import { Card, HStack, Spinner, VStack } from '@chakra-ui/react';
 import { BsTrainFrontFill } from 'react-icons/bs';
 import { GiPathDistance } from 'react-icons/gi';
 import { BsFuelPumpFill } from 'react-icons/bs';
 import { BsSpeedometer2 } from 'react-icons/bs';
+import { useQuery } from '@tanstack/react-query';
+import { trainInfosQuery } from '@/queries/train';
+
 const TrainCard = () => {
+  const { data, isPending, isError } = useQuery(trainInfosQuery);
+
+  if (isPending || isError) return <Spinner />;
   return (
     <Card.Root width={'320px'}>
       <Card.Body>
@@ -11,10 +17,10 @@ const TrainCard = () => {
           <BsTrainFrontFill size={'20%'} />
           <VStack>
             <HStack>
-              <BsFuelPumpFill /> : 60
+              <BsFuelPumpFill /> : {data.fuel}
             </HStack>
             <HStack>
-              <GiPathDistance />: 1230km
+              <GiPathDistance />: {data.kilometer}km
             </HStack>
             <HStack>
               <BsSpeedometer2 />: 250km/h
